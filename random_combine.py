2d6a6ea006a153127d4d3f97848435a65542d995
LTK72 <125452849+LTK72@users.noreply.github.com>
2023-03-23 16:10:59 +0800

    updated, file input need to be changed

--- random_combine.py
+++ random_combine.py
@@ -3,7 +3,7 @@ import numpy as np
 from PIL import Image as im
 from os.path import join
 
-def random_combine(imgIn, imgGT, minSize, maxSize):
+def random_combine(imgIn = "sample_data/1 (43)_B.jpeg", imgGT = "sample_data/1 (43).jpeg", minSize = 500, maxSize =1000, count=0):
     # imgIn = im.open(join("pix2pix-pytorch-master", imgIn)) 
     # imgGT = im.open(join("pix2pix-pytorch-master", imgGT)) 
 
@@ -11,21 +11,23 @@ def random_combine(imgIn, imgGT, minSize, maxSize):
     imgGT = im.open(join(imgGT))
 
     width, height = imgIn.size
+    print(width, height)
     left = random.randint(0, width - minSize)
     right = left + random.randint(minSize, maxSize)
     top = random.randint(0, height - minSize)
     bottom = top + random.randint(minSize, maxSize)
-    
+    print(left, right, top, bottom)
+
     imgIn = np.array(imgIn)
     imgGT = np.array(imgGT)
     
-    imgIn[left:right, top:bottom, :] = 0
-    imgGT[0:left, :, :] = 0
-    imgGT[right:, :, :] = 0
-    imgGT[:, 0:top, :] = 0
-    imgGT[:, bottom:, :] = 0
+    imgIn[top:bottom, left:right, :] = 0
+    imgGT[:, 0:left, :] = 0
+    imgGT[:, right:, :] = 0
+    imgGT[0:top, :, :] = 0
+    imgGT[bottom:, :, :] = 0
     
     result = imgGT + imgIn
     data = im.fromarray(result)
-    
-    return data.save("dummy.png")
+    filename = str(i) + ".png"
+    return data.save(filename), im.open(filename).show()
